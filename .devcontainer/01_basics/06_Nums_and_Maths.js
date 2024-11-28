f7a51ae4bcc2b7f106481b6bd36c8ba24b740d80
Usama604 <mu7871580@gmail.com>
2024-11-28 10:31:01 +0000

    Nums and Maths

---
+++ .devcontainer/01_basics/06_Nums_and_Maths.js
@@ -0,0 +1,16 @@
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
+console.log(Math.ceil(6.1));
+console.log(Math.floor(6.9));
+console.log(Math.min(2, 4, 6));
+console.log(Math.max(1, 3, 7));
+
+console.log(Math.random());
+
+console.log(Math.random()*10 +1);
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) )
