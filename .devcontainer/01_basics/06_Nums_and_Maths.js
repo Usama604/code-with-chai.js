6efae4a1459b2ff8f50584d273fd2e52bb7c7a40
Usama604 <mu7871580@gmail.com>
2024-11-28 10:36:30 +0000

    Nums and Maths

--- .devcontainer/01_basics/06_Nums_and_Maths.js
+++ .devcontainer/01_basics/06_Nums_and_Maths.js
@@ -1,9 +1,27 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100); 
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+
+const otherNumber = 23.9080;
+console.log(otherNumber.toPrecision(3));
+
+const malik = 100000;
+console.log(malik.toLocaleString());
+
+// +++++++++++++++++++ Maths ++++++++++++++
+
+console.log(Math);
 console.log(Math.abs(-4));
-console.log(Math.round(4.5));
-console.log(Math.ceil(6.1));
-console.log(Math.floor(6.9));
-console.log(Math.min(2, 4, 6));
-console.log(Math.max(1, 3, 7));
+console.log(Math.round(4.4));
+console.log(Math.ceil(4.1));
+console.log(Math.floor(4.0));
+console.log(Math.min(4, 5, 8));
+console.log(Math.max(3, 2, 1));
 
 console.log(Math.random());
 
