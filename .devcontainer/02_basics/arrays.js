664e116bc6e64c273550ce3b259bb9a0a3219ac9
Usama604 <mu7871580@gmail.com>
2024-11-29 10:40:36 +0000

    Arrays

---
+++ .devcontainer/02_basics/arrays.js
@@ -0,0 +1,35 @@
+// Array 
+const Arr = [0,1, 2, 3, 4, 5]
+
+
+const Arr2 = ["malik", "ali"]
+
+const Arr3= new Array( 1, 2, 3, 4);
+
+//console.log(Arr[2])
+
+// Arrays method 
+
+Arr3.push(6) // add number in end 
+Arr3.pop(6)  // remove number in end 
+
+Arr3.unshift // add number in first 
+Arr3.shift   // remove number in first 
+
+// console.log(Arr3.includes(9))
+// console.log(Arr3.indexOf(3))
+
+const newArr = Arr3.join()
+//console.log(Arr3)
+//console.log(newArr)
+
+// slice , splice
+
+console.log("A ", Arr)
+
+const myn1 = Arr.slice(1,3)
+
+console.log(myn1)
+
+const myn2 = Arr.splice(1,3);
+console.log(myn2);
