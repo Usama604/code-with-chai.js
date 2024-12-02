52106c6897f701373c8699ef719a161f36100d8f
Usama604 <mu7871580@gmail.com>
2024-12-02 18:08:47 +0000

    Functions

--- .devcontainer/03_basics/01_functions.js
+++ .devcontainer/03_basics/01_functions.js
@@ -30,3 +30,31 @@ function myname(username) {
 }
 
 console.log(myname("malik"))
+
+function calculatecartprice(val1, val2, ...num1){
+    return num1
+}
+console.log(calculatecartprice(200, 400, 500, 600, 800));
+
+// objects convert into functions
+
+const user = {
+    user: "malik",
+    age: 17
+
+}
+
+function handleobject(anyobject){
+    console.log(`user ${anyobject.user} and age ${anyobject.age}`);
+}
+handleobject(user)
+
+
+// Arrays convert into functions
+
+const array = [100, 200, 300, 400]
+
+function newarray(getarray){
+    return getarray[2]
+}
+console.log(newarray(array));
