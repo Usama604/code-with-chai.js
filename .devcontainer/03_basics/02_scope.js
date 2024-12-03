4465f4c4ada4754f51b996215feb540c868e902b
Usama604 <mu7871580@gmail.com>
2024-12-03 17:35:35 +0000

    scope

---
+++ .devcontainer/03_basics/02_scope.js
@@ -0,0 +1,51 @@
+// Scope
+
+// var c = 300
+let a = 300
+if (true) {
+    let a = 10;
+    const b = 20;
+    var c = 30
+    console.log("inner:", a)
+    
+}
+
+//console.log(a);
+// console.log(b);
+//console.log(c);
+
+function one() {
+    const username = "malik"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    two()
+   // console.log(website);
+}
+one()
+
+
+if (true) {
+    const username = "malik"
+    if (true) {
+        const website = " youtube"
+        console.log(username + website);
+    }
+}
+
+// ************************* interesting example *******************
+
+console.log(one(4));
+function one(num) {
+    return num + 1
+}
+
+
+
+const addtwo = function(num) {
+    return num + 1
+}
+addtwo()
