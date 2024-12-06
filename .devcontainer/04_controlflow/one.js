84a0d7cb7a61590d5ed72623697e940a6073094d
Usama604 <mu7871580@gmail.com>
2024-12-06 18:32:22 +0000

    controlflow

---
+++ .devcontainer/04_controlflow/one.js
@@ -0,0 +1,57 @@
+// control flow
+
+// const time = 12
+
+// if (time !== 12) {
+//     console.log("run code");
+    
+// }
+// else{
+//     console.log("not run");
+    
+// }
+
+// < , > , <= , >= , == , != , === , !==
+
+// const score = 200
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`userpower: ${power}`);
+// }
+// console.log(`userpower: ${power}`)
+
+
+// const balance = 1000
+// if (balance > 500) console.log("text"), console.log("text2"); not readable code
+
+
+
+// const balance = 1000
+// if (balance < 500) {
+//     console.log("greater than 500");
+    
+// } else if (balance < 700) {
+//     console.log(" greater than 700");
+    
+// }
+// else if (balance < 900) {
+//     console.log("greater than 900");
+    
+// }else{
+//     console.log("less than 1200");
+    
+// }
+
+
+const IDcard = true
+const ATMcard = true
+if (IDcard && ATMcard && 2==2) {
+    console.log("g mara pass idcard bee hai aur atm card bee hai");
+} 
+
+const collegestudent = false
+const universitystudent = true
+if (collegestudent || universitystudent) {
+    console.log("i am a university student");
+    
+}
