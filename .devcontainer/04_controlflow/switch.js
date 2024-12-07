6996d0ffb72489d8ab304b5183363231cbe163c6
Usama604 <mu7871580@gmail.com>
2024-12-07 07:57:38 +0000

    Switch

---
+++ .devcontainer/04_controlflow/switch.js
@@ -0,0 +1,64 @@
+// Switch
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const months = 3
+
+switch (months) {
+    case 1:
+    console.log("jan");
+        break;
+
+    case 2:
+    console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+
+    default:
+        console.log("defult");
+        
+        break;
+}
+
+
+const days = 7
+
+switch (days) {
+    case 1:
+    console.log("sunday");
+        break;
+
+    case 2:
+    console.log("mondsy");
+        break;
+    case 3:
+        console.log("thuesday");
+        break;
+
+    case 4:
+        console.log("wenesday");
+        break;
+    case 5:
+        console.log("thrisday");
+        break;
+
+    case 6:
+        console.log("friday");
+        break;
+    case 7:
+        console.log("sarahday");
+        break;            
+    default:
+        console.log("defult");
+        
+        break;
+}
