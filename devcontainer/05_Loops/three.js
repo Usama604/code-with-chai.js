3cabbc941d1dd08e4c61fa61c0f8e00104fe3ce8
Usama604 <mu7871580@gmail.com>
2024-12-08 09:52:48 +0000

    forofloop

---
+++ devcontainer/05_Loops/three.js
@@ -0,0 +1,44 @@
+// for loops
+
+// ["", "", ]
+// [{}, {}, {}]
+
+const myarr = [1,2, 3, 4, 5];
+
+for (const num of myarr) {
+    //console.log(num);
+}
+
+const greetings = "Hello world" 
+
+for (const greet of greetings) {
+    //console.log(`value : ${greet}`);
+    
+}
+
+// Maps
+
+const map = new Map()
+map.set(`pak`, `pakistan`)
+map.set(`in`, `indian`)
+map.set(`fr`, `franc`)
+
+console.log(map);
+
+for (const [key, val] of map) {
+    console.log(key, val);
+    
+}
+
+// object not use in (for of loop)
+const myobj = {
+    'pak': 'pakistan',
+    'in': 'indian'
+}
+console.log(myobj);
+
+for (const key of myobj) {
+    console.log(key);
+    
+}
+
